4033434856475bf7fe69ccee55ec6dfc7ef2731b
Sunghyun Cho <hey@cho.sh>
2025-04-30 11:54:58 +0900

    style: Update layout config logo with flag emojis

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -4,10 +4,7 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" aria-label="Logo">
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        대한민국 WWDC 위너 모임
+        🇰🇷 WWDC Winner 모임
       </>
     ),
   },

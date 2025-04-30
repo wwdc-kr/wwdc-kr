6597817bf2dfd7e63b13d180eafe500b43c71264
Sunghyun Cho <hey@cho.sh>
2025-04-30 11:56:27 +0900

    style: Update text content in home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,13 +3,12 @@ import Link from 'next/link'
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
+      <h1 className="mb-4 text-2xl font-bold">🇰🇷 대한민국 WWDC 위너 모임</h1>
       <p className="text-fd-muted-foreground">
-        You can open{' '}
         <Link href="/docs" className="text-fd-foreground font-semibold underline">
-          /docs
+          톺아보기
         </Link>{' '}
-        and see the documentation.
+        페이지를 방문해서 더 알아보세요.
       </p>
     </main>
   )

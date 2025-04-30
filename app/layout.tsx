e4d1fc8e0a3da05ff190dd51bacae29266b35697
Sunghyun Cho <hey@cho.sh>
2025-04-30 10:10:21 +0900

    style: Add custom font family to root element and html tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,19 @@
 import './global.css'
 import { RootProvider } from 'fumadocs-ui/provider'
-import { Inter } from 'next/font/google'
 import type { ReactNode } from 'react'
-
-const inter = Inter({
-  subsets: ['latin'],
-})
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
-      <body className="flex flex-col min-h-screen">
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"
+        />
+      </head>
+      <body className="flex flex-col min-h-screen font-sans">
         <RootProvider
           search={{
             options: {

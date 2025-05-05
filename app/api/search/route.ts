de4097e5b4550f8ac9b3546c7ac59db17f39e019
Jaesung Lee <chic0815@konkuk.ac.kr>
2025-05-06 02:14:13 +0900

    [FIX] 빌드 에러 수정

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,8 +1,8 @@
-import { source } from '@/lib/source'
+import { docsSource } from '@/lib/source'
 import { createFromSource } from 'fumadocs-core/search/server'
 
 export const dynamic = 'force-static'
 export const revalidate = false
 
 // Use staticGET for static exports
-export const { staticGET: GET } = createFromSource(source)
+export const { staticGET: GET } = createFromSource(docsSource)

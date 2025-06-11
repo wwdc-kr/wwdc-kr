6d1c0523f504602d84ef2e9442f57c31e104d4d5
x_0o0 <chic0815@konkuk.ac.kr>
2025-06-12 01:23:44 +0900

    [DEL] WWDC25 Watch Party 안내 종료 (#15)

--- app/events/watchparty-wwdc25/page.tsx
+++ app/events/watchparty-wwdc25/page.tsx
@@ -1,9 +1,5 @@
-import { redirect } from 'next/navigation'
+import EndedEvent from '@/app/components/EndedEvent'
 
-export default function WatchPartyPage() {
-  redirect('https://swift-students.notion.site/WWDC25-Watch-Party-1f64297016f5812ab4b5e6ef9a846e60?pvs=4')
-}
-
-// 정적 내보내기를 위한 설정
-export const dynamic = 'force-static'
-export const revalidate = false 
+export default function WatchPartyWWDC25() {
+  return <EndedEvent />
+} 

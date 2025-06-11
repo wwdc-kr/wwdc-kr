1e35df7c952a2a7d52e2c540a7f22e32a964ed90
Jaesung Lee <chic0815@konkuk.ac.kr>
2025-06-12 01:18:15 +0900

    [DEL] WWDC25 Watch Party 안내 종료

---
+++ app/components/EndedEvent.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+export default function EndedEvent() {
+  return (
+    <main className="flex-1 flex flex-col items-center justify-center text-center min-h-[50vh]">
+      <div className="space-y-4">
+        <p className="text-xl font-medium whitespace-pre-line">
+          🌉 종료된 행사 입니다 🌉
+        </p>
+        <p className="text-base text-gray-600">
+          행사에 참석해주신 분들께 좋은 추억이 되길 바라며 감사의 말씀 드립니다 🙂
+        </p>
+      </div>
+    </main>
+  )
+} 

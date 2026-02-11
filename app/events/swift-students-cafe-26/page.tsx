57b966a923e77725127f58ee8ccf1d804669666b
x_0o0 <chic0815@konkuk.ac.kr>
2026-02-11 20:57:38 +0800

    [MOD] Swift Students Cafe 26 종료 안내 (#27)

--- app/events/swift-students-cafe-26/page.tsx
+++ app/events/swift-students-cafe-26/page.tsx
@@ -1,9 +1,5 @@
-import { redirect } from 'next/navigation'
+import EndedEvent from '@/app/components/ended-event'
 
-export default function WatchPartyPage() {
-  redirect('https://luma.com/vcs8ohev')
+export default function SwiftStudentsCafe26() {
+  return <EndedEvent />
 }
-
-// 정적 내보내기를 위한 설정
-export const dynamic = 'force-static'
-export const revalidate = false

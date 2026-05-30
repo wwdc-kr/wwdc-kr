ec8925126aa151b4305b19aad4bcb9e5570aaa0a
x_0o0 <chic0815@konkuk.ac.kr>
2026-05-31 03:46:15 +0900

    [ADD] Swift Students Cafe 26 포항 - Iterate on Your Idea 리다이렉트 추가 (#30)

---
+++ app/events/swift-students-cafe-26/pohang/iterate-on-your-idea/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from 'next/navigation'
+export default function SwiftStudentsCafe26PohangIterateOnYourIdea() {
+  redirect('https://luma.com/6t1yha0t')
+}
+// 정적 내보내기를 위한 설정
+export const dynamic = 'force-static'
+export const revalidate = false
